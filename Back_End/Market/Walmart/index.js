3ad57e906e6de8cca03bc3d8be578adb621b4b20
ntjoar <nathan.tjoar@gmail.com>
2020-11-06 02:45:18 -0800

    Query for simple axios getting works now, need to investigate parsing bug further

--- Back_End/Market/Walmart/index.js
+++ Back_End/Market/Walmart/index.js
@@ -1,65 +1,83 @@
 const Market = require('../../model/Market');
 const Item = require('../../model/Item');
-const axios = require('axios');
+const axios = require('axios').default;
 
-async function getReq (query) {
-    var itemArr = []
+function getSearchItemUrl(query) {
+    return `https://www.walmart.com/search/?query=${query}`;
+}
 
-    const response = await axios.get('https://www.walmart.com/search/?query=' + query)
-    var data = response.data
+async function fetch(link) {
+    return await axios.get(link);
+}
+
+async function getHtml(link) {
+    return (await fetch(link)).data;
+}
+
+async function getResults (query) {
+    var itemArr = [];
+
+    let data = await getHtml(getSearchItemUrl(query));
+
     // TODO: Edit this variable
-    var ulStart = data.indexOf('<ul class="search-result-gridview-items four-items" data-automation-id="search-result-gridview-items">')
-    var i = 0
+    // Need to update, Walmart seems to have updated theirs...
+    var ulStart = data.indexOf('<ul class="search-result-gridview-items four-items" data-automation-id="search-result-gridview-items">');
+    var i = 0;
     while(i < 10) {
-        var objInitId = ulStart + data.substr(ulStart).indexOf('<li class="Grid-col')
-        var objEndId = objInitId + data.substr(objInitId).indexOf('</li>')
+        var objInitId = ulStart + data.substr(ulStart).indexOf('<li class="Grid-col');
+        var objEndId = objInitId + data.substr(objInitId).indexOf('</li>');
 
         // Get name
-        var prodId = objInitId + data.substr(objInitId).indexOf('Product Title')
-        var nameId = prodId + data.substr(prodId).indexOf('<span>') + 6
-        var nameLen = data.substr(nameId).indexOf('</span>')
-        var checkNameLen = data.substr(nameId).indexOf('"')
-        var minNameLen = -1
+        var prodId = objInitId + data.substr(objInitId).indexOf('Product Title');
+        var nameId = prodId + data.substr(prodId).indexOf('<span>') + 6;
+        var nameLen = data.substr(nameId).indexOf('</span>');
+        var checkNameLen = data.substr(nameId).indexOf('"');
+        var minNameLen = -1;
         if(checkNameLen < nameLen) {
-            minNameLen = checkNameLen
+            minNameLen = checkNameLen;
         } else {
-            minNameLen = nameLen
+            minNameLen = nameLen;
         }
-        var tempName = data.substr(nameId, minNameLen)
-        name = tempName.replace(/(<([^>]+)>)/ig,"")
+        var tempName = data.substr(nameId, minNameLen);
+        name = tempName.replace(/(<([^>]+)>)/ig,"");
 
         // Get link
-        var linkId = objInitId + data.substr(objInitId).indexOf('<a href="/ip/') + 9
-        var linkLen = data.substr(linkId).indexOf('"')
-        var tempLink = 'https://walmart.com' + data.substr(linkId, linkLen)
-        var link = tempLink.replace(/(<([^>]+)>)/ig,"")
+        var linkId = objInitId + data.substr(objInitId).indexOf('<a href="/ip/') + 9;
+        var linkLen = data.substr(linkId).indexOf('"');
+        var tempLink = 'https://walmart.com' + data.substr(linkId, linkLen);
+        var link = tempLink.replace(/(<([^>]+)>)/ig,"");
 
         // Get Price
-        var curPricePos = objInitId + data.substr(objInitId).indexOf('Current Price')
-        var price = curPricePos + data.substr(curPricePos).indexOf('$') + 1
-        var priceLen = data.substr(price).indexOf('"')
-        var checkPriceLen = data.substr(price).indexOf('<')
-        var minPriceLen = -1
+        var curPricePos = objInitId + data.substr(objInitId).indexOf('Current Price');
+        var price = curPricePos + data.substr(curPricePos).indexOf('$') + 1;
+        var priceLen = data.substr(price).indexOf('"');
+        var checkPriceLen = data.substr(price).indexOf('<');
+        var minPriceLen = -1;
         if(checkPriceLen < priceLen) {
-            minPriceLen = checkPriceLen
+            minPriceLen = checkPriceLen;
         } else {
-            minPriceLen = priceLen
+            minPriceLen = priceLen;
         }
-        var tempFinalPrice = data.substr(price, minPriceLen)
-        var finalPrice = tempFinalPrice.replace(/(<([^>]+)>)/ig,"")
+        var tempFinalPrice = data.substr(price, minPriceLen);
+        var finalPrice = tempFinalPrice.replace(/(<([^>]+)>)/ig,"");
 
-        ulStart = objEndId
-        i += 1
-        itemArr.push(new Item(name, link, finalPrice))
+        ulStart = objEndId;
+        i += 1;
+        console.log(name);
+        itemArr.push(new Item(name, link, finalPrice));
     }
-    return itemArr
+    return itemArr;
 }
 
 module.exports = {
     search: async function(query) {
-        //TODO(ntjoar): Implement for Walmart
-        let items = await getReq(query)
-        return new Market('Walmart', 'www.walmart.com', items);
+        try{
+            let items = await getResults(query)
+            return new Market('Walmart', 'www.walmart.com', items);
+        }
+        catch (e) {
+            return new Market('Walmart', 'www.walmart.com', []);
+        }
     }
 }
 
