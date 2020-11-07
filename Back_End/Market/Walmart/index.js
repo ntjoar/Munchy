cf0ee83da88bf93b9156dd3869e413afe36dc2c5
ntjoar <nathan.tjoar@gmail.com>
2020-11-07 13:58:36 -0800

    Cleaned up favicon by having it default to error, added nodemon. GET on Walmart returning 412 for some reason

--- Back_End/Market/Walmart/index.js
+++ Back_End/Market/Walmart/index.js
@@ -1,6 +1,8 @@
 const Market = require('../../model/Market');
 const Item = require('../../model/Item');
 const axios = require('axios').default;
+const cheerio = require('cheerio');
+const https = require('https') // Delete this later
 
 function getSearchItemUrl(query) {
     return `https://www.walmart.com/search/?query=${query}`;
@@ -15,58 +17,23 @@ async function getHtml(link) {
 }
 
 async function getResults (query) {
-    var itemArr = [];
+    var data = (await axios.get('https://www.walmart.com/')).data;
+    console.log(data);
 
-    let data = await getHtml(getSearchItemUrl(query));
 
-    // TODO: Edit this variable
-    // Need to update, Walmart seems to have updated theirs...
-    var ulStart = data.indexOf('<ul class="search-result-gridview-items four-items" data-automation-id="search-result-gridview-items">');
-    var i = 0;
-    while(i < 10) {
-        var objInitId = ulStart + data.substr(ulStart).indexOf('<li class="Grid-col');
-        var objEndId = objInitId + data.substr(objInitId).indexOf('</li>');
+    let html = await getHtml(getSearchItemUrl(query));
+    const $ = cheerio.load(html);
 
-        // Get name
-        var prodId = objInitId + data.substr(objInitId).indexOf('Product Title');
-        var nameId = prodId + data.substr(prodId).indexOf('<span>') + 6;
-        var nameLen = data.substr(nameId).indexOf('</span>');
-        var checkNameLen = data.substr(nameId).indexOf('"');
-        var minNameLen = -1;
-        if(checkNameLen < nameLen) {
-            minNameLen = checkNameLen;
-        } else {
-            minNameLen = nameLen;
-        }
-        var tempName = data.substr(nameId, minNameLen);
-        name = tempName.replace(/(<([^>]+)>)/ig,"");
 
-        // Get link
-        var linkId = objInitId + data.substr(objInitId).indexOf('<a href="/ip/') + 9;
-        var linkLen = data.substr(linkId).indexOf('"');
-        var tempLink = 'https://walmart.com' + data.substr(linkId, linkLen);
-        var link = tempLink.replace(/(<([^>]+)>)/ig,"");
+    console.log(searchResults.text());
+    searchResults.each((i, element) => {
+        let elementCheerio = cheerio.load(element);
+        console.log(elementCheerio.text());
 
-        // Get Price
-        var curPricePos = objInitId + data.substr(objInitId).indexOf('Current Price');
-        var price = curPricePos + data.substr(curPricePos).indexOf('$') + 1;
-        var priceLen = data.substr(price).indexOf('"');
-        var checkPriceLen = data.substr(price).indexOf('<');
-        var minPriceLen = -1;
-        if(checkPriceLen < priceLen) {
-            minPriceLen = checkPriceLen;
-        } else {
-            minPriceLen = priceLen;
-        }
-        var tempFinalPrice = data.substr(price, minPriceLen);
-        var finalPrice = tempFinalPrice.replace(/(<([^>]+)>)/ig,"");
-
-        ulStart = objEndId;
-        i += 1;
+        let name = elementCheerio('div.search-result-product-title > .visuallyhidden').text().trim();
         console.log(name);
-        itemArr.push(new Item(name, link, finalPrice));
-    }
-    return itemArr;
+    })
+    return items;
 }
 
 module.exports = {
