4c9e3a0ca2a1bc6af7e73ee2654aac433e23de3f
ntjoar <nathan.tjoar@gmail.com>
2020-11-12 12:34:53 -0800

    Temporary Bypass on Walmart. Need to handle redirects and bot detection

--- Back_End/Market/Walmart/index.js
+++ Back_End/Market/Walmart/index.js
@@ -2,10 +2,10 @@ const Market = require('../../model/Market');
 const Item = require('../../model/Item');
 const axios = require('axios').default;
 const cheerio = require('cheerio');
-const https = require('https') // Delete this later
+const { http, https } = require('follow-redirects');
 
 function getSearchItemUrl(query) {
-    return `https://www.walmart.com/search/?query=${query}`;
+    return `https://www.walmart.com/browse/food/chocolate/976759_1096070_1224976?&search_redirect=true&redirectQuery=${query}`;
 }
 
 async function fetch(link) {
@@ -17,22 +17,22 @@ async function getHtml(link) {
 }
 
 async function getResults (query) {
-    var data = (await axios.get('https://www.walmart.com/')).data;
-    console.log(data);
+    let items = [];
+    // console.log(getSearchItemUrl(query));
+
+    // let html = await axios.get(getSearchItemUrl(query));
+    // console.log(getSearchItemUrl(query));
+    // const $ = cheerio.load(html);
 
 
-    let html = await getHtml(getSearchItemUrl(query));
-    const $ = cheerio.load(html);
+    // console.log(searchResults.text());
+    // searchResults.each((i, element) => {
+    //     let elementCheerio = cheerio.load(element);
+    //     console.log(elementCheerio.text());
 
-
-    console.log(searchResults.text());
-    searchResults.each((i, element) => {
-        let elementCheerio = cheerio.load(element);
-        console.log(elementCheerio.text());
-
-        let name = elementCheerio('div.search-result-product-title > .visuallyhidden').text().trim();
-        console.log(name);
-    })
+    //     let name = elementCheerio('div.search-result-product-title > .visuallyhidden').text().trim();
+    //     console.log(name);
+    // })
     return items;
 }
 
