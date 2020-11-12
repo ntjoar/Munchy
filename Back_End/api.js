c9be1239937e1b763f62e03af1889136fd3c6c4d
articbear1999 <jacobchenlin@gmail.com>
2020-11-12 12:35:49 -0800

    Data parsing for radius, longitude, latitude

--- Back_End/api.js
+++ Back_End/api.js
@@ -16,7 +16,30 @@ app.use(cors())
 
 app.get('/:query', async (req, res) => {
     let query = req.params.query;
-
+    let splitQuery = query.split('&')
+    //default radius is 0
+    let radius = "0"
+    let longitude = "default"
+    let latitude = "default"
+    for (const words of splitQuery){
+        if(words.includes("radius="))
+        {
+            radius = words.substring(7)
+        }
+        if(words.includes("lo="))
+        {
+            longitude = words.substring(3)
+        }
+        if(words.includes("la="))
+        {
+            latitude = words.substring(3)
+        }
+    }
+    if(longitude == "default" ||latitude == "default")
+    {
+        console.log("no input for location")
+        radius = "0"
+    }
     marketDataArr = []
 
     for (const [key, module] of marketApi.entries()) {
