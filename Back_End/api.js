fad53e798af72231e94a70ef9912166e50766160
articbear1999 <jacobchenlin@gmail.com>
2020-11-12 13:14:01 -0800

    modified code so query works when paired with radius and coordinates

--- Back_End/api.js
+++ Back_End/api.js
@@ -23,25 +23,34 @@ app.get('/:query', async (req, res) => {
     let radius = "0"
     let longitude = "default"
     let latitude = "default"
+    let itemsList = []
+    //Parse through query to check for radius, longitude, latitude
     for (const words of splitQuery){
         if(words.includes("radius="))
         {
             radius = words.substring(7)
         }
-        if(words.includes("lo="))
+        else if(words.includes("lo="))
         {
             longitude = words.substring(3)
         }
-        if(words.includes("la="))
+        else if(words.includes("la="))
         {
             latitude = words.substring(3)
         }
+        //put all actual searchable items into a list for now
+        else{
+            itemsList.push(words)
+        }
     }
+    
     if(longitude == "default" ||latitude == "default")
     {
         console.log("no input for location")
         radius = "0"
     }
+    //set the query to the first item in list for now as well, this will change later
+    query = itemsList[0]
     marketDataArr = []
 
     for (const [key, module] of marketApi.entries()) {
